2e0dbce4d5ddcc6b3a7df7d019de3408ced8b5a3
Alex de Mulder <alexdemulder@gmail.com>
2015-04-02 10:45:44 +0200

    added background subtract

--- cvtest.py
+++ cvtest.py
@@ -4,6 +4,8 @@ import cv2
 import picamera
 import picamera.array
 
+fgbg = cv2.BackgroundSubtractorMOG()
+
 with picamera.PiCamera() as camera:
     with picamera.array.PiRGBArray(camera) as stream:
         camera.resolution = (320, 240)
@@ -11,7 +13,9 @@ with picamera.PiCamera() as camera:
         while True:
             camera.capture(stream, 'bgr', use_video_port=True)
             # stream.array now contains the image data in BGR order
-            cv2.imshow('frame', stream.array)
+
+            fgmask = fgbg.apply(stream.array)
+            cv2.imshow('frame', fgmask)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             # reset the stream before the next capture
